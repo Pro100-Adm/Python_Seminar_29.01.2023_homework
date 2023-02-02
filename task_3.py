5ee9d50cc5a0128c7f28af7f82d4922cf2439a4c
Pro100-Adm <kirow.alesha@yandex.ru>
2023-02-02 13:34:10 +0300

    Seminar 29.01.2023 homework

--- task_3.py
+++ task_3.py
@@ -1,8 +1,8 @@
 my_list = [1, 5, 7.3, "Sample text", False, None]
 
-def get_types_of_list_lements(my_list):
+def get_types_of_list_elements(my_list):
     for element in my_list:
         print(f"{type(element)}")
     return 0
 
-get_types_of_list_lements(my_list)
+get_types_of_list_elements(my_list)
