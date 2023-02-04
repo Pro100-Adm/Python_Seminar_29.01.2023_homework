0faf51cf55bae011a11aa69b5d005705414f67fb
Pro100-Adm <kirow.alesha@yandex.ru>
2023-02-04 10:14:55 +0300

    Seminar 29.01.2023 homework

--- task_5.py
+++ task_5.py
@@ -1,10 +1,7 @@
 def get_words(user_str):
     words = user_str.split(" ")
     for i in range(len(words)):
-        if len(words[i])>10:
-            print(f"{str(i+1)}. {words[i][:10]}")
-        else:
-            print(f"{str(i+1)}. {words[i]}")
+        print(f"{str(i+1)}. {words[i][:10]}")
 
 get_words(input("Введите слова через пробел: "))
 
